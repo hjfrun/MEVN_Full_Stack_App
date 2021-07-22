427f154d343b433937f1950574c1b0775646311e
Jianfeng He <jianfeng.he@oliverwyman.com>
2021-07-22 23:32:14 +0800

    deploy test

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,5 +4,12 @@ module.exports = {
   ],
   devServer: {
     proxy: "http://localhost:5000"
+  },
+  chainWebpack: config => {
+    config.plugin('html')
+      .tap(args => {
+        args[0].title = '影视一栏'
+        return args
+      })
   }
 }
